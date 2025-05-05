bebbd07e39938ba71704587f52edf9e92331e920
karci.meliss@gmail.com <karci.meliss@gmail.com>
2025-05-05 03:24:49 +0300

    Commit

---
+++ app/src/main/java/com/meliskarci/contactappwithandroidjetpackcompose/ui/detail/components/ContactRow.kt
@@ -0,0 +1,87 @@
+package com.meliskarci.contactappwithandroidjetpackcompose.ui.detail.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ContactRow(
+    icon: ImageVector,
+    title: String,
+    data: String,
+    isNumber: Boolean
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Left Icon
+        Icon(
+            imageVector = icon,
+            contentDescription = title,
+            tint = Color.Gray,
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+
+        // Contact Information
+        Column {
+            Text(
+                text = title,
+                style = TextStyle(
+                    color = Color.Gray,
+                    fontSize = 14.sp
+                )
+            )
+            Text(
+                text = data,
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 16.sp
+                )
+            )
+        }
+        Spacer(modifier = Modifier.width(110.dp))
+
+        // Action Icons
+        if (isNumber) {
+            Icon(
+                imageVector = Icons.Default.MailOutline,
+                contentDescription = "Message",
+                tint  = Color(0xFF00ADB5),
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Icon(
+                imageVector = Icons.Default.Phone,
+                contentDescription = "Call",
+                tint  = Color(0xFF00ADB5)
+            )
+        } else {
+            Icon(
+                imageVector = Icons.Default.Email,
+                contentDescription = "Email",
+                tint  = Color(0xFF00ADB5)
+            )
+        }
+    }
+}
